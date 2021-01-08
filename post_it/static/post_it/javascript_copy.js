198c81064b88d4e38c8d5be917b288f536690c5a
constantin-kuehne <70445955+constantin-kuehne@users.noreply.github.com>
2021-01-08 14:52:04 +0100

    Repaired write collaboration

--- post_it/static/post_it/javascript_copy.js
+++ post_it/static/post_it/javascript_copy.js
@@ -161,12 +161,15 @@ $(function() {
         }
         return false;
     });
-
-    $('.textarea').bind('input propertychange', function(event) {
-        var message = postItSyncEle(event.target.parentElement, "write");
-        savesock.send(JSON.stringify(message));
-        return false;
-    });
+    
+    function writeJQuery(){
+        $('.textarea').bind('input propertychange', function(event) {
+            var message = postItSyncEle(event.target.parentElement, "write");
+            savesock.send(JSON.stringify(message));
+            return false;
+        });
+    };
+    writeJQuery();
 
     $(document).on('click', '#saveButton', function(event){
         var listPostIts = {};
@@ -202,216 +205,216 @@ $(function() {
         savesock.send(JSON.stringify(message))
         return false
     });
-});
 
-function createPostIt(user) {
-    var max = 0;
-    $('.draggable').each(function() {
-        max = Math.max(this.id, max);
-    });
-
-    var container = document.createElement("div")
-    var author = document.createElement("div")
-    var header = document.createElement("h3")
-    var header_text = document.createTextNode(user)
-    var text_area = document.createElement("textarea")
-    var thumbtacks_img = document.createElement("img")
-
-    var options_menu = document.createElement("div")
-    var options_menu_img = document.createElement("img")
-    var options_menu_list = document.createElement("ul")
-
-    var options_menu_list_delete = document.createElement("li")
-    var options_menu_list_change_color = document.createElement("li")
-    var options_menu_list_change_ground = document.createElement("li")
-
-    var delete_button = document.createElement("button")
-
-    var color_change_div = document.createElement("div")
-    var change_color_button = document.createElement("button")
-    var color_change_list = document.createElement("ul")
-    
-    var lightblue_list = document.createElement("li")
-    var pink_list = document.createElement("li")
-    var yellow_list = document.createElement("li")
-    var lightgreen_list = document.createElement("li")
-    var orange_list = document.createElement("li")
-    var purple_list = document.createElement("li")
-    var olive_list = document.createElement("li")
-    
-    var lightblue = document.createElement("button")
-    var pink = document.createElement("button")
-    var yellow = document.createElement("button")
-    var lightgreen = document.createElement("button")
-    var orange = document.createElement("button")
-    var purple = document.createElement("button")
-    var olive = document.createElement("button")
-
-    var ground_change_div = document.createElement("div")
-    var change_ground_button = document.createElement("button")
-    var ground_change_list = document.createElement("ul")
-
-    var up_list = document.createElement("li")
-    var down_list = document.createElement("li")
-    
-    var up = document.createElement("button")
-    var down = document.createElement("button")
-
-    var break_line = document.createElement("br")
-    var filler_1 = document.createTextNode("")
-    var filler_2 = document.createTextNode("")
-    var filler_3 = document.createTextNode("")
-    var filler_4 = document.createTextNode("")
-    var filler_5 = document.createTextNode("")
-    var filler_6 = document.createTextNode("")
-    var filler_7 = document.createTextNode("")
-    var filler_8 = document.createTextNode("")
-    var filler_9 = document.createTextNode("")
-    var filler_10 = document.createTextNode("")
-    var filler_11 = document.createTextNode("")
-    var filler_12 = document.createTextNode("")
-    var filler_13 = document.createTextNode("")
-    var filler_14 = document.createTextNode("")
-    var filler_15 = document.createTextNode("")
-    var filler_16 = document.createTextNode("")
-    var filler_17 = document.createTextNode("")
-    var filler_18 = document.createTextNode("")
-
-    container.className = "draggable"
-    container.id = max + 1
-    container.style.zIndex = 0
-    container.style.backgroundColor = "lightblue"
-    container.style.top = "150px"
-    author.className = "author"
-    text_area.className = "textarea"
-    text_area.style.backgroundColor = "lightblue"
-    thumbtacks_img.className = "thumbtacks"
-    thumbtacks_img.src = thumbtackUrl
-    options_menu_img.src = optionsUrl
-    options_menu.className = "dropdown"
-    options_menu.addEventListener("click", function(e) {
-            e = e || window.event;
-            var target = e.target,
-                text = target.textContent || target.innerText;
-            target.parentElement.classList.toggle("show")
+    function createPostIt(user) {
+        var max = 0;
+        $('.draggable').each(function() {
+            max = Math.max(this.id, max);
         });
-    delete_button.className = "listbutton deleteButton"
-    delete_button.innerHTML = "Delete"
-
-    lightblue.className = "listbutton"
-    lightblue.setAttribute("onClick", "changeColor(this)")
-    lightblue.innerHTML = "lightblue"
-    pink.className = "listbutton"
-    pink.setAttribute("onClick", "changeColor(this)")
-    pink.innerHTML = "pink"
-    yellow.className = "listbutton"
-    yellow.setAttribute("onClick", "changeColor(this)")
-    yellow.innerHTML = "yellow"
-    lightgreen.className = "listbutton"
-    lightgreen.setAttribute("onClick", "changeColor(this)")
-    lightgreen.innerHTML = "lightgreen"
-    orange.className = "listbutton"
-    orange.setAttribute("onClick", "changeColor(this)")
-    orange.innerHTML = "orange"
-    purple.className = "listbutton"
-    purple.setAttribute("onClick", "changeColor(this)")
-    purple.innerHTML = "purple"
-    olive.className = "listbutton"
-    olive.setAttribute("onClick", "changeColor(this)")
-    olive.innerHTML = "olive"
-
-    lightblue_list.appendChild(lightblue)
-    pink_list.appendChild(pink)
-    yellow_list.appendChild(yellow)
-    lightgreen_list.appendChild(lightgreen)
-    orange_list.appendChild(orange)
-    purple_list.appendChild(purple)
-    olive_list.appendChild(olive)
     
-    color_change_div.className = "dropdowncolor"
-    change_color_button.className = "listbutton"
-    change_color_button.innerHTML = "Color >"
-    change_color_button.setAttribute("onClick", "showDropdownColor(this)")
+        var container = document.createElement("div")
+        var author = document.createElement("div")
+        var header = document.createElement("h3")
+        var header_text = document.createTextNode(user)
+        var text_area = document.createElement("textarea")
+        var thumbtacks_img = document.createElement("img")
     
-    up.className = "listbutton"
-    up.setAttribute("onClick", "indexUp(this)")
-    up.innerHTML = "Up +"
-    down.className = "listbutton"
-    down.setAttribute("onClick", "indexDown(this)")
-    down.innerHTML = "Down -"
-
-    up_list.appendChild(up)
-    down_list.appendChild(down)
-
-    ground_change_div.className = "dropdownground"
-    change_ground_button.className = "listbutton"
-    change_ground_button.innerHTML = "Ground >"
-    change_ground_button.setAttribute("onClick", "showDropdownGround(this)")
-
-
-    header.appendChild(header_text)
+        var options_menu = document.createElement("div")
+        var options_menu_img = document.createElement("img")
+        var options_menu_list = document.createElement("ul")
     
-    color_change_div.appendChild(filler_11)
-    color_change_div.appendChild(change_color_button)
-    color_change_div.appendChild(filler_12)
-
-    color_change_list.appendChild(lightblue_list)
-    color_change_list.appendChild(pink_list)
-    color_change_list.appendChild(yellow_list)
-    color_change_list.appendChild(lightgreen_list)
-    color_change_list.appendChild(orange_list)
-    color_change_list.appendChild(purple_list)
-    color_change_list.appendChild(olive_list)
-
-    color_change_div.appendChild(color_change_list)
-    color_change_div.appendChild(filler_13)
-
-    ground_change_div.appendChild(change_ground_button)
-
-    ground_change_list.appendChild(up_list)
-    ground_change_list.appendChild(down_list)
+        var options_menu_list_delete = document.createElement("li")
+        var options_menu_list_change_color = document.createElement("li")
+        var options_menu_list_change_ground = document.createElement("li")
     
-    ground_change_div.appendChild(ground_change_list)
-
-    options_menu_list_delete.appendChild(delete_button)
-    options_menu_list_change_color.appendChild(color_change_div)
-    options_menu_list_change_color.appendChild(filler_10)
-    options_menu_list_change_ground.appendChild(ground_change_div)
-
-    options_menu.appendChild(options_menu_img)
-    options_menu.appendChild(filler_4)
-    options_menu_list.appendChild(filler_6)
-    options_menu_list.appendChild(options_menu_list_delete)
-    options_menu_list.appendChild(filler_7)
-    options_menu_list.appendChild(options_menu_list_change_color)
-    options_menu_list.appendChild(filler_8)
-    options_menu_list.appendChild(options_menu_list_change_ground)
-    options_menu_list.appendChild(filler_9)
-    options_menu.appendChild(options_menu_list)
-    options_menu.appendChild(filler_5)
+        var delete_button = document.createElement("button")
     
-    author.appendChild(filler_14)
-    author.appendChild(thumbtacks_img)
-    author.appendChild(filler_15)
-    author.appendChild(options_menu)
-    author.appendChild(filler_16)
-
-    author.appendChild(break_line)
-    author.appendChild(filler_17)
-    author.appendChild(header)
-    author.appendChild(filler_18)
-
-    container.appendChild(filler_1)
-    container.appendChild(author)
-    container.appendChild(filler_2)
-    container.appendChild(text_area)
-    container.appendChild(filler_3)
-
-    document.body.appendChild(container)
-    update()
-    return container
-};
-
+        var color_change_div = document.createElement("div")
+        var change_color_button = document.createElement("button")
+        var color_change_list = document.createElement("ul")
+        
+        var lightblue_list = document.createElement("li")
+        var pink_list = document.createElement("li")
+        var yellow_list = document.createElement("li")
+        var lightgreen_list = document.createElement("li")
+        var orange_list = document.createElement("li")
+        var purple_list = document.createElement("li")
+        var olive_list = document.createElement("li")
+        
+        var lightblue = document.createElement("button")
+        var pink = document.createElement("button")
+        var yellow = document.createElement("button")
+        var lightgreen = document.createElement("button")
+        var orange = document.createElement("button")
+        var purple = document.createElement("button")
+        var olive = document.createElement("button")
+    
+        var ground_change_div = document.createElement("div")
+        var change_ground_button = document.createElement("button")
+        var ground_change_list = document.createElement("ul")
+    
+        var up_list = document.createElement("li")
+        var down_list = document.createElement("li")
+        
+        var up = document.createElement("button")
+        var down = document.createElement("button")
+    
+        var break_line = document.createElement("br")
+        var filler_1 = document.createTextNode("")
+        var filler_2 = document.createTextNode("")
+        var filler_3 = document.createTextNode("")
+        var filler_4 = document.createTextNode("")
+        var filler_5 = document.createTextNode("")
+        var filler_6 = document.createTextNode("")
+        var filler_7 = document.createTextNode("")
+        var filler_8 = document.createTextNode("")
+        var filler_9 = document.createTextNode("")
+        var filler_10 = document.createTextNode("")
+        var filler_11 = document.createTextNode("")
+        var filler_12 = document.createTextNode("")
+        var filler_13 = document.createTextNode("")
+        var filler_14 = document.createTextNode("")
+        var filler_15 = document.createTextNode("")
+        var filler_16 = document.createTextNode("")
+        var filler_17 = document.createTextNode("")
+        var filler_18 = document.createTextNode("")
+    
+        container.className = "draggable"
+        container.id = max + 1
+        container.style.zIndex = 0
+        container.style.backgroundColor = "lightblue"
+        container.style.top = "150px"
+        author.className = "author"
+        text_area.className = "textarea"
+        text_area.style.backgroundColor = "lightblue"
+        thumbtacks_img.className = "thumbtacks"
+        thumbtacks_img.src = thumbtackUrl
+        options_menu_img.src = optionsUrl
+        options_menu.className = "dropdown"
+        options_menu.addEventListener("click", function(e) {
+                e = e || window.event;
+                var target = e.target,
+                    text = target.textContent || target.innerText;
+                target.parentElement.classList.toggle("show")
+            });
+        delete_button.className = "listbutton deleteButton"
+        delete_button.innerHTML = "Delete"
+    
+        lightblue.className = "listbutton"
+        lightblue.setAttribute("onClick", "changeColor(this)")
+        lightblue.innerHTML = "lightblue"
+        pink.className = "listbutton"
+        pink.setAttribute("onClick", "changeColor(this)")
+        pink.innerHTML = "pink"
+        yellow.className = "listbutton"
+        yellow.setAttribute("onClick", "changeColor(this)")
+        yellow.innerHTML = "yellow"
+        lightgreen.className = "listbutton"
+        lightgreen.setAttribute("onClick", "changeColor(this)")
+        lightgreen.innerHTML = "lightgreen"
+        orange.className = "listbutton"
+        orange.setAttribute("onClick", "changeColor(this)")
+        orange.innerHTML = "orange"
+        purple.className = "listbutton"
+        purple.setAttribute("onClick", "changeColor(this)")
+        purple.innerHTML = "purple"
+        olive.className = "listbutton"
+        olive.setAttribute("onClick", "changeColor(this)")
+        olive.innerHTML = "olive"
+    
+        lightblue_list.appendChild(lightblue)
+        pink_list.appendChild(pink)
+        yellow_list.appendChild(yellow)
+        lightgreen_list.appendChild(lightgreen)
+        orange_list.appendChild(orange)
+        purple_list.appendChild(purple)
+        olive_list.appendChild(olive)
+        
+        color_change_div.className = "dropdowncolor"
+        change_color_button.className = "listbutton"
+        change_color_button.innerHTML = "Color >"
+        change_color_button.setAttribute("onClick", "showDropdownColor(this)")
+        
+        up.className = "listbutton"
+        up.setAttribute("onClick", "indexUp(this)")
+        up.innerHTML = "Up +"
+        down.className = "listbutton"
+        down.setAttribute("onClick", "indexDown(this)")
+        down.innerHTML = "Down -"
+    
+        up_list.appendChild(up)
+        down_list.appendChild(down)
+    
+        ground_change_div.className = "dropdownground"
+        change_ground_button.className = "listbutton"
+        change_ground_button.innerHTML = "Ground >"
+        change_ground_button.setAttribute("onClick", "showDropdownGround(this)")
+    
+    
+        header.appendChild(header_text)
+        
+        color_change_div.appendChild(filler_11)
+        color_change_div.appendChild(change_color_button)
+        color_change_div.appendChild(filler_12)
+    
+        color_change_list.appendChild(lightblue_list)
+        color_change_list.appendChild(pink_list)
+        color_change_list.appendChild(yellow_list)
+        color_change_list.appendChild(lightgreen_list)
+        color_change_list.appendChild(orange_list)
+        color_change_list.appendChild(purple_list)
+        color_change_list.appendChild(olive_list)
+    
+        color_change_div.appendChild(color_change_list)
+        color_change_div.appendChild(filler_13)
+    
+        ground_change_div.appendChild(change_ground_button)
+    
+        ground_change_list.appendChild(up_list)
+        ground_change_list.appendChild(down_list)
+        
+        ground_change_div.appendChild(ground_change_list)
+    
+        options_menu_list_delete.appendChild(delete_button)
+        options_menu_list_change_color.appendChild(color_change_div)
+        options_menu_list_change_color.appendChild(filler_10)
+        options_menu_list_change_ground.appendChild(ground_change_div)
+    
+        options_menu.appendChild(options_menu_img)
+        options_menu.appendChild(filler_4)
+        options_menu_list.appendChild(filler_6)
+        options_menu_list.appendChild(options_menu_list_delete)
+        options_menu_list.appendChild(filler_7)
+        options_menu_list.appendChild(options_menu_list_change_color)
+        options_menu_list.appendChild(filler_8)
+        options_menu_list.appendChild(options_menu_list_change_ground)
+        options_menu_list.appendChild(filler_9)
+        options_menu.appendChild(options_menu_list)
+        options_menu.appendChild(filler_5)
+        
+        author.appendChild(filler_14)
+        author.appendChild(thumbtacks_img)
+        author.appendChild(filler_15)
+        author.appendChild(options_menu)
+        author.appendChild(filler_16)
+    
+        author.appendChild(break_line)
+        author.appendChild(filler_17)
+        author.appendChild(header)
+        author.appendChild(filler_18)
+    
+        container.appendChild(filler_1)
+        container.appendChild(author)
+        container.appendChild(filler_2)
+        container.appendChild(text_area)
+        container.appendChild(filler_3)
+    
+        document.body.appendChild(container)
+        update()
+        writeJQuery()
+        return container
+    };
+});
 
 function deletePostIt(elmnt){
     elmnt.remove()
